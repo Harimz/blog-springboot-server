d6220e941ea8072b1b1195151a2a75b6011352d5
harimz <harimzermeno@gmail.com>
2026-01-02 20:41:33 -0800

    removed /

--- src/main/java/com/spring/blog/config/SecurityConfig.java
+++ src/main/java/com/spring/blog/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "https://logbook-blog.netlify.app",
-                "https://logbook-blog.online/"
+                "https://logbook-blog.online"
         ));
 
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
